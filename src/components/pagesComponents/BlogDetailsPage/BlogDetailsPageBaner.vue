53b627baf407b37138264cd84c63d955d9047c00
Andrew Erofeev <eroha210@mail.ru>
2024-05-19 21:19:08 +0300

    first commit

---
+++ src/components/pagesComponents/BlogDetailsPage/BlogDetailsPageBaner.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="banner-details center"></section>
+</template>
+
+<script>
+export default {
+  name: "BlogDetailsPageBaner",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.banner-details {
+  background-image: url("../../../assets/images/blog-details-header.jpg");
+  height: 356px;
+}
+.center {
+  padding-left: calc(50% - 1200px / 2);
+  padding-right: calc(50% - 1200px / 2);
+}
+</style>
